6cd099e77262f7e57b835798c5b5ddf8b0e3fb83
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-26 19:47:51 -0500

    add oauth20

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5001/api/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Find or create user
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        if (!user) {
+          user = new User({
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            password: "", // Google login doesn’t require a password
+          });
+          await user.save();
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        return done(null, { token });
+      } catch (err) {
+        return done(err, false);
+      }
+    }
+  )
+);
