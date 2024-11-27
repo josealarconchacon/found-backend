168214eb7b895e1e1b9fabe30c8be7f7804fbb5e
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-26 19:46:49 -0500

    only register with school emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,20 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 // Register User
 exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+
+    // Check if email is a valid school email
+    if (!email.endsWith("@school.edu")) {
+      return res
+        .status(400)
+        .json({ message: "Only school emails are allowed" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ username, email, password: hashedPassword });
     await user.save();
@@ -31,3 +40,16 @@ exports.login = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// Google authentication route
+exports.googleAuth = passport.authenticate("google", {
+  scope: ["profile", "email"],
+});
+
+// Google callback route
+(exports.googleAuthCallback = passport.authenticate("google", {
+  failureRedirect: "/",
+})),
+  (req, res) => {
+    res.json({ token: req.user.token });
+  };
