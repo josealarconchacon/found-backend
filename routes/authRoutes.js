a6e58868d25ac5528466a77b363ade5f27964749
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-26 19:47:21 -0500

    update route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
 const router = express.Router();
+const authController = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/google", authController.googleAuth);
+router.get("/google/callback", authController.googleAuthCallback);
 
 module.exports = router;
