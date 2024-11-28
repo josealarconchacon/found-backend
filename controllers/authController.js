fc7ebbedf5fdb46a2ef3c49e6172201fffc90d97
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-28 15:58:49 -0500

    update file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,58 +1,3 @@
-// const User = require("../models/User");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-// const passport = require("passport");
-
-// // Register User
-// exports.register = async (req, res) => {
-//   try {
-//     const { username, email, password } = req.body;
-
-//     // Check if email is a valid school email
-//     if (!email.endsWith("@school.edu")) {
-//       return res
-//         .status(400)
-//         .json({ message: "Only school emails are allowed" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = new User({ username, email, password: hashedPassword });
-//     await user.save();
-//     res.status(201).json({ message: "User registered" });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// };
-
-// // Login User
-// exports.login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
-//     if (!user || !(await bcrypt.compare(password, user.password))) {
-//       return res.status(400).json({ message: "Invalid credentials" });
-//     }
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "1h",
-//     });
-//     res.json({ token });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// };
-
-// // Google authentication route
-// exports.googleAuth = passport.authenticate("google", {
-//   scope: ["profile", "email"],
-// });
-
-// // Google callback route
-// (exports.googleAuthCallback = passport.authenticate("google", {
-//   failureRedirect: "/",
-// })),
-//   (req, res) => {
-//     res.json({ token: req.user.token });
-//   };
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -63,7 +8,7 @@ exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    if (!email.endsWith("@school.edu")) {
+    if (!email.endsWith(".edu")) {
       return res
         .status(400)
         .json({ message: "Only school emails are allowed" });
