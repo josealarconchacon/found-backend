c034bcaaac5d822c6fac37a2ff239039f5b670d0
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-29 22:55:11 -0500

    connect to db

--- config/db.js
+++ config/db.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Default to 'found' database if not specified in MONGO_URI
     const dbURI = process.env.MONGO_URI || "mongodb://localhost:27017/found";
-    // Log a warning if MONGO_URI is not set
     if (!process.env.MONGO_URI) {
       console.warn(
         "MONGO_URI not defined. Using default: 'mongodb://localhost:27017/found'"
       );
     }
-    // Connect to MongoDB without deprecated options
+    // Connect to MongoDB
     await mongoose.connect(dbURI);
     console.log("MongoDB connected to database:", dbURI);
   } catch (err) {
